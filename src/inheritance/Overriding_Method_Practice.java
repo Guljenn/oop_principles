2ce5dedfde03db5947b8c9899a694623d7849b35
Guljenn <jennincekara@gmail.com>
2022-04-16 15:39:47 -0500

    added more practices

---
+++ src/inheritance/Overriding_Method_Practice.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class Overriding_Method_Practice {
+    public static void main(String[] args) {
+        Tester tester = new Tester();
+
+        tester.eat();
+        tester.code();
+        tester.sleep();
+        tester.walk();
+        tester.learn();
+    }
+}
--- src/inheritance/Tester.java
+++ src/inheritance/Tester.java
@@ -7,9 +7,24 @@ public class Tester extends Person {
     public Tester (String fName, String lName, int age, String gender){
         super(fName, lName, age, gender);
     }
-   public void code(){
+   public void code() {
        System.out.println("Tester codes");
-
-
    }
-}
+        public void eat(){
+            System.out.println("Tester eats");
+
+       }
+
+       public void sleep(){
+           System.out.println("Tester sleeps");
+       }
+
+       public void walk(){
+           System.out.println("Tester walks");
+       }
+
+       public void learn(){
+           System.out.println("Tester learns");
+       }
+   }
+
